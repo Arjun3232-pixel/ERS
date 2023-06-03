55f4c11ce9ee92898d07c0f0b1ccff7d60c2d66c
Arjun3232-pixel <77073565+Arjun3232-pixel@users.noreply.github.com>
2023-06-03 18:46:55 +0530

    Add files via upload

---
+++ Privacy.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Windows.Input;
+using WebApplication4.Pages.Employees;
+
+namespace WebApplication4.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public EmpReg emps = new EmpReg();
+
+        public void OnGet()
+        {
+            
+        }
+        public void OnPost()
+        {
+            emps.EmpID = Request.Form["EmpID"];
+            emps.EmpName = Request.Form["EmpName"];
+            emps.Email = Request.Form["Email"];
+            emps.DoB = Request.Form["DoB"];
+            emps.Phone = Request.Form["Phone"];
+            emps.Department = Request.Form["Department"];
+
+            try
+            {
+                
+                String connectionString = "Data Source=ARJUN;Initial Catalog=Employees;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "INSERT INTO EmpReg (EmpID,EmpName,Email,DoB,Phone,Department) VALUES (@EmpID, @EmpName, @Email, @DoB, @Phone, @Department);";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@EmpID", emps.EmpID);
+                        command.Parameters.AddWithValue("@EmpName",emps.EmpName);
+                        command.Parameters.AddWithValue("@Email", emps.Email);
+                        command.Parameters.AddWithValue("@DoB", emps.DoB);
+                        command.Parameters.AddWithValue("@Phone", emps.Phone);
+                        command.Parameters.AddWithValue("@Department", emps.Department);
+
+                        command.ExecuteNonQuery();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+        }
+    }
+
+}
